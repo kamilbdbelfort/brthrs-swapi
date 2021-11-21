577b7c6475e49d6359c4b49d16b7a1ca6603198f
kamilbdbelfort <k.beelidebelfort@hotmail.com>
2021-11-21 11:00:06 +0100

    create router folder, added/defined routes in de index.js, updated filmsRouter.js

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ app.use(corsMiddleWare());
 const bodyParserMiddleWare = express.json();
 app.use(bodyParserMiddleWare);
 
-// Routes
+// Define routers
+const FilmsRouter = require("./routers/filmsRouter");
+const PeopleRouter = require("./routers/peopleRouter");
+const PlanetsRouter = require("./routers/planetsRouter");
+
+// Use routers
+app.use("/", FilmsRouter);
+app.use("/people", PeopleRouter);
+app.use("/planets", PlanetsRouter);
 
 // Listen for connections on specified port (default is port 4000)
 const { PORT } = require("./config/constants");
